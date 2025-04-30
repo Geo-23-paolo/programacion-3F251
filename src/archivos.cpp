13533170931fca1eed1592923977aef99031da89
Geo-23-paolo <a24110209@live.ceti.mx>
2025-04-30 00:15:48 +0000

    practica 5

--- src/archivos.cpp
+++ src/archivos.cpp
@@ -2,6 +2,13 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+struct Persona
+{
+    char nombre[12];
+    int edad;
+};
+
 int main()
 {
     ofstream archivoSalida("archivo.txt");
@@ -11,7 +18,7 @@ int main()
         return 1;
         // Finalizar con error
     }
-    cout<<"escribiendo archivo" << endl;
+    cout << "escribiendo archivo" << endl;
     archivoSalida << "Hola mundo este es mi primer archivo" << endl;
     archivoSalida << "Hola mundo este es mi primer archivo" << endl;
     archivoSalida << "Hola mundo este es mi primer archivo" << endl;
@@ -26,11 +33,32 @@ int main()
         return 1;
         // Finalizar con error
     }
-    cout<<"leyendo archivo" << endl;
+    cout << "leyendo archivo" << endl;
     string linea;
-    while(getline(archivoEntrada, linea)){
-        cout<< linea <<endl;
+    while (getline(archivoEntrada, linea))
+    {
+        cout << linea << endl;
     }
     archivoEntrada.close();
+
+    //-------------------------------------------------------------------
+    // Escritura de archivos:
+    ofstream archivoSalidaBinario("archivo.bin", ios::binary);
+    if (!archivoSalidaBinario)
+    {
+        cerr << "Error al leer archivo" << endl;
+        return 1;
+        // Finalizar con error
+    }
+    Persona p1 = {"Juan", 20};
+    Persona p2 = {"Pedro", 30};
+    Persona p3 = {"Maria", 40};
+    archivoSalidaBinario.write((char*)(&p1), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p2), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p3), sizeof(Persona));
+
+    archivoSalidaBinario.close();
+
+
     return 0;
 }
